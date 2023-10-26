289cc0054bb1b96d26262823926fa4d0feab78fa
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2023-10-26 16:23:47 +0800

    feat : output file
    1. mapping file
    2. encoder file

--- src/main/java/GUID/GUID/App.java
+++ src/main/java/GUID/GUID/App.java
@@ -18,6 +18,7 @@ import tw.edu.ym.guid.client.field.Sex;
 import tw.edu.ym.guid.client.field.TWNationalId;
 import tw.edu.ym.guid.client.hashcode.GuidHashcodeGenerator;
 import java.io.FileReader;
+import java.util.stream.IntStream;
 
 public class App {
 	static String GUID_SERVER_URL = "https://120.126.47.138:8443";
@@ -30,40 +31,57 @@ public class App {
 		String csvFile = "112students.csv"; // 從命令行參數中獲取CSV文件的路徑和文件名稱
 		String line = "";
 		String csvSplitBy = ","; // CSV文件中的分隔符
-		String outputCsvFile = "newStudents.csv"; // 輸出的CSV文件名稱
+		String outputMappingCsvFile = "mapping.csv"; // 輸出的CSV文件名稱
+		String outputEnCoderCsvFile = "enCoder.csv"; // 輸出的CSV文件名稱
 
+		int Name_index = 1;
+		int[] needToEnCoder = new int[] { 1, 4 };
+		
 		try (BufferedReader br = new BufferedReader(new FileReader(csvFile));
-				BufferedWriter bw = new BufferedWriter(new FileWriter(outputCsvFile))) {
+				BufferedWriter bwM = new BufferedWriter(new FileWriter(outputMappingCsvFile));
+				BufferedWriter bwE = new BufferedWriter(new FileWriter(outputEnCoderCsvFile));) {
 			int index = 0;
-			
-			while ((line = br.readLine()) != null) {
-                String[] data = line.split(csvSplitBy);
-                data = Arrays.copyOf(data, data.length + 1);
 
-                if (index != 0) {
-                	String firstName = String.valueOf(data[1].charAt(0));
-					String lastName = data[1].substring(1);
+			while ((line = br.readLine()) != null) {
+				String[] data = line.split(csvSplitBy);
+				data = Arrays.copyOf(data, data.length + 1);
+
+				if (index != 0) {
+					String firstName = String.valueOf(data[Name_index].charAt(0));
+					String lastName = data[Name_index].substring(1);
 					Sex sex = Sex.MALE;
 					Birthday birthday = new Birthday(2001, 1, 1);
 					String nationalId = "A123456789";
 					String piiString = enCoder(firstName, lastName, sex, birthday, nationalId);
 
-                    data[data.length - 1] = piiString;
-                } else {
-                    data[data.length - 1] = "GUID";
-                }
+					data[data.length - 1] = piiString;
+				} else {
+					data[data.length - 1] = "GUID";
+				}
 
-                // 將CSV數據顯示在控制台上
-                for (String item : data) {
-                    System.out.print(item + " ");
-                    // 將數據寫入CSV文件
-                    bw.write(item + ",");
-                }
-                bw.write("\n"); // 換行
-                System.out.println(); // 換行
-                index++;
-            }           
-			System.out.println("CSV文件已成功生成：" + outputCsvFile);
+				for (int i = 0; i < data.length -1; i++) {
+				    final int i_index = i; // 聲明為 final 或者 effectively final
+
+					if(IntStream.of(needToEnCoder).anyMatch(x -> x == i_index)) {
+						bwE.write(data[data.length - 1]+",");
+					}else {
+						bwE.write(data[i]+",");
+					}
+				}
+
+				// 將CSV數據顯示在控制台上
+				for (String item : data) {
+					System.out.print(item + " ");
+					// 將數據寫入CSV文件
+					bwM.write(item + ",");
+				}
+				
+				bwM.write("\n"); // 換行
+				bwE.write("\n"); // 換行
+				System.out.println(); // 換行
+				index++;
+			}
+			System.out.println("CSV文件已成功生成：" + outputMappingCsvFile);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
